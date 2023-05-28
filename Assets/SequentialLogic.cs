3e622d23892f820000117094f8bc50bf370d2ef3
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2023-05-28 13:34:43 -0700

    Progress: cleaner desc implementaiton

--- Assets/SequentialLogic.cs
+++ Assets/SequentialLogic.cs
@@ -5,76 +5,131 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Events;
 
+public class GameController
+{
+    public ChoiceController choice { get; private set; }
+    public RegularTextController reg { get; private set; }
+    public SpriteLogic sprite { get; private set; }
+
+    public GameController(GameObject choiceObj, GameObject regObj, GameObject spriteObj)
+    {
+        // Get all necessary controlling and logic objects
+        choice = choiceObj.GetComponent(typeof(ChoiceController)) as ChoiceController;
+        reg = regObj.GetComponent(typeof(RegularTextController)) as RegularTextController;
+        sprite = spriteObj.GetComponent(typeof(SpriteLogic)) as SpriteLogic;
+    }
+
+    public void DestroyAllPrompts()
+    {
+        choice.DestroyChoices();
+        reg.DestroyChoices();
+    }
+}
+
+class TestDescision1
+{
+    GameController c;
+    UnityAction NextChoice1;
+    UnityAction NextChoice2;
+    UnityAction NextChoice3;
+    public TestDescision1(
+        GameController c, UnityAction NextChoice1, UnityAction NextChoice2, UnityAction NextChoice3
+    )
+    {
+        this.c = c;
+        this.NextChoice1 = NextChoice1;
+        this.NextChoice2 = NextChoice2;
+        this.NextChoice3 = NextChoice3;
+    }
+
+    void GoodChoice()
+    {
+        c.DestroyAllPrompts();
+        c.sprite.airPollution = 0;
+        c.sprite.UpdateSky();
+        NextChoice1();
+    }
+
+    void OkChoice()
+    {
+        c.DestroyAllPrompts();
+        c.sprite.airPollution = 2;
+        c.sprite.UpdateSky();
+        NextChoice2();
+    }
+
+    void BadChoice()
+    {
+        c.DestroyAllPrompts();
+        c.sprite.airPollution = 4;
+        c.sprite.UpdateSky();
+        NextChoice3();
+    }
+
+    string GetPrompt()
+    { return "Test Descision: Air Pollution"; }
+
+    string[] GetChoiceStrs()
+    { return new string[] { "Good Choice", "Ok Choice", "Bad Choice" }; }
+
+    UnityAction[] GetActions()
+    { return new UnityAction[] { GoodChoice, OkChoice, BadChoice }; }
+
+    public void InitChoices()
+    {
+        c.DestroyAllPrompts();
+        c.choice.InitChoices(GetPrompt(), GetChoiceStrs(), GetActions());
+    }
+}
+
+class TestRegPrompt1
+{
+    GameController c;
+    UnityAction NextAction;
+    public TestRegPrompt1(GameController c, UnityAction NextAction)
+    {
+        this.c = c;
+        this.NextAction = NextAction;
+    }
+
+    public void InitChoices()
+    {
+        c.DestroyAllPrompts();
+        c.reg.InitChoices("Here is a starting prompt", NextAction);
+    }
+}
+
 public class SequentialLogic : MonoBehaviour
 {
 
     // Initing control objects
-    public GameObject choiceControllerObj;
-    public GameObject regularTextControllerObj;
-    public GameObject spriteLogicObj;
+    public GameObject choiceObj;
+    public GameObject regObj;
+    public GameObject spriteObj;
 
-    ChoiceController choiceController;
-    RegularTextController regularTextController;
-    SpriteLogic spriteLogic;
+    GameController c;
 
-    // Last Prompt
-    string regularPrompt2 = "Some result text";
+    TestDescision1 td1;
+    TestRegPrompt1 tr1;
 
-    void DoneTest()
+    void TerminalAction()
     {
         Debug.Log("Done Testing");
 
-        regularTextController.DestroyChoices();
-    }
-
-    // sky choice callbacks
-    void GoodSkyChoice()
-    {
-        choiceController.DestroyChoices();
-
-        spriteLogic.airPollution = 0;
-        spriteLogic.UpdateSky();
-        regularTextController.InitChoices(regularPrompt2, DoneTest);
-    }
-    void OkSkyChoice()
-    {
-        choiceController.DestroyChoices();
-
-        spriteLogic.airPollution = 1;
-        spriteLogic.UpdateSky();
-        regularTextController.InitChoices(regularPrompt2, DoneTest);
-    }
-    void BadSkyChoice()
-    {
-        choiceController.DestroyChoices();
-
-        spriteLogic.airPollution = 2;
-        spriteLogic.UpdateSky();
-        regularTextController.InitChoices(regularPrompt2, DoneTest);
-    }
-
-    void RegPrompt1ToChoicePrompt1()
-    {
-        regularTextController.DestroyChoices();
-
-        string choice1Prompt = "Pick something";
-        string[] choice1Chocies = {"Good Sky", "Ok Sky", "Bad Sky"};
-        UnityAction[] actions = {GoodSkyChoice, OkSkyChoice, BadSkyChoice};
-        choiceController.InitChoices(choice1Prompt, choice1Chocies, actions);
+        c.DestroyAllPrompts();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        // Get all necessary controlling and logic objects
-        choiceController = choiceControllerObj.GetComponent(typeof(ChoiceController)) as ChoiceController;
-        regularTextController = regularTextControllerObj.GetComponent(typeof(RegularTextController)) as RegularTextController;
-        spriteLogic = spriteLogicObj.GetComponent(typeof(SpriteLogic)) as SpriteLogic;
+        // create gamecontroller object
+        c = new GameController(choiceObj, regObj, spriteObj);
 
-        // remove the dialogues
-        choiceController.DestroyChoices();
-        regularTextController.DestroyChoices();
+        // create TestDescision1 with each choice having callback of TerminalAction
+        td1 = new TestDescision1(c, TerminalAction, TerminalAction, TerminalAction);
+        tr1 = new TestRegPrompt1(c, td1.InitChoices);
 
-        regularTextController.InitChoices("This is a test", RegPrompt1ToChoicePrompt1);
+        // start with first prompt
+        tr1.InitChoices();
     }
 }
--- Assets/SpriteLogic.cs
+++ Assets/SpriteLogic.cs
@@ -3,33 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpriteLogic : MonoBehaviour
-{
-
+{  
+    // ------------------------------------------------------------
     // sky tracking
     public int airPollution = 0;
-    public const int GOOD_SKY = 0;
-    public const int OK_SKY = 1;
-    public const int BAD_SKY = 2;
     public GameObject Sky;
 
     [ContextMenu("UpdateSky")]
-    public void UpdateSky()
-    {
-        SpriteChanger SkySC = Sky.GetComponent<SpriteChanger>();
-        SkySC.ChangeSprite(airPollution);
-    }
+    public void UpdateSky() { Sky.GetComponent<SpriteChanger>().ChangeSprite((int)(airPollution / 2)); }
     
     // testing methods
-    [ContextMenu("IncrSky")]
-    public void IncrSky()
-    {
-        ++airPollution;
-    }
-    [ContextMenu("DecrSky")]
-    public void DecrSky()
-    {
-        --airPollution;
-    }
+    [ContextMenu("IncrAirPol")]
+    public void IncrAirPol() { airPollution = System.Math.Min(5, airPollution + 1); }
+    [ContextMenu("DecrAirPol")]
+    public void DecrAirPol() { airPollution = System.Math.Max(0, airPollution - 1); }
+
+    // ------------------------------------------------
 
     // scene tracking counters
     public int farmlandGrowth = 0;
